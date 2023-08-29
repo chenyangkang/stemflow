646de88216093ac1cc7c44ab236d969019d722d4
chenyangkang <chenyangkang24@outlook.com>
2023-08-29 16:44:23 +0800

    Aug29

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
     long_description = "\n" + fh.read()
 
-VERSION = '0.0.2'
+VERSION = '0.0.3'
 DESCRIPTION = 'AdaSTEM model for daily abundance estimation using eBird citizen science data'
 LONG_DESCRIPTION = 'TBD'
 
