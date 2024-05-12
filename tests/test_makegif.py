cd5d07bc668d5e83d1ff184b09273fe4b0f3e03f
chenyangkang <chenyangkang24@outlook.com>
2024-05-12 14:37:38 +0800

    fix make gif test data bugs

--- tests/test_makegif.py
+++ tests/test_makegif.py
@@ -43,31 +43,31 @@ def test_make_gif():
     shutil.rmtree(tmp_dir)
 
 
-# def test_make_gif_changing_ranges():
-#     fake_data_ = fake_data[(fake_data["x"] >= 0) & (fake_data["y"] >= 10) & (fake_data["DOY"] >= 20)].copy()
+def test_make_gif_changing_ranges():
+    fake_data_ = fake_data[(fake_data["x"] >= 0) & (fake_data["y"] >= 10) & (fake_data["DOY"] >= 20)].copy()
 
-#     tmp_dir = "./stemflow_test_make_gif"
-#     if not os.path.exists(tmp_dir):
-#         os.mkdir(tmp_dir)
+    tmp_dir = "./stemflow_test_make_gif"
+    if not os.path.exists(tmp_dir):
+        os.mkdir(tmp_dir)
 
-#     make_sample_gif(
-#         fake_data_,
-#         os.path.join(tmp_dir, "FTR_IPT_dat_changing_ranges.gif"),
-#         col="dat",
-#         log_scale=False,
-#         Spatio1="x",
-#         Spatio2="y",
-#         Temporal1="DOY",
-#         figsize=(18, 9),
-#         xlims=None,
-#         ylims=None,
-#         grid=True,
-#         xtick_interval=None,
-#         ytick_interval=None,
-#         lng_size=10,
-#         lat_size=10,
-#         dpi=100,
-#         fps=10,
-#     )
+    make_sample_gif(
+        fake_data_,
+        os.path.join(tmp_dir, "FTR_IPT_dat_changing_ranges.gif"),
+        col="dat",
+        log_scale=False,
+        Spatio1="x",
+        Spatio2="y",
+        Temporal1="DOY",
+        figsize=(18, 9),
+        xlims=None,
+        ylims=None,
+        grid=True,
+        xtick_interval=None,
+        ytick_interval=None,
+        lng_size=10,
+        lat_size=10,
+        dpi=100,
+        fps=10,
+    )
 
-#     shutil.rmtree(tmp_dir)
+    shutil.rmtree(tmp_dir)
